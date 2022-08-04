b1d9c435d8e6f1a3f3d51827cd541ddfdd492c52
MrGunflame <git@robbsrv.de>
2022-08-04 23:17:05 +0200

    Docs: Update local module documentation

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -1,4 +1,7 @@
-//! `!Send` implementations
+//! Synchronization primitives single-threaded contexts.
+//!
+//! This module provides provides the same synchronization primitives, but for a single-threaded
+//! context.
 mod notify;
 
 pub use notify::{Notified, Notify};
